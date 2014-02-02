05e6f1f9a6acfdd6c2a56866d54da2ba7957fc25
Mithrandir0x <mithrandir0x@gmail.com>
2014-02-02 12:11:55 +0100

    Changed some code comments for pycco

--- P04/valueIterationAgents.py
+++ P04/valueIterationAgents.py
@@ -4,37 +4,35 @@ from learningAgents import ValueEstimationAgent
 
 class ValueIterationAgent(ValueEstimationAgent):
   """
-      * Please read learningAgents.py before reading this.*
-
-      A ValueIterationAgent takes a Markov decision process
-      (see mdp.py) on initialization and runs value iteration
-      for a given number of iterations using the supplied
-      discount factor.
+  A ValueIterationAgent takes a Markov decision process
+  (see mdp.py) on initialization and runs value iteration
+  for a given number of iterations using the supplied
+  discount factor.
   """
   def __init__(self, mdp, discount = 0.9, iterations = 100):
     """
-      Your value iteration agent should take an mdp on
-      construction, run the indicated number of iterations
-      and then act according to the resulting policy.
+    Your value iteration agent should take an mdp on
+    construction, run the indicated number of iterations
+    and then act according to the resulting policy.
     
-      Some useful mdp methods you will use:
-          mdp.getStates()
-          mdp.getPossibleActions(state)
-          mdp.getTransitionStatesAndProbs(state, action)
-          mdp.getReward(state, action, nextState)
+    Some useful mdp methods you will use:
+        
+        mdp.getStates()
+        mdp.getPossibleActions(state)
+        mdp.getTransitionStatesAndProbs(state, action)
+        mdp.getReward(state, action, nextState)
     """
     self.mdp = mdp
     self.discount = discount
     self.iterations = iterations
-    self.values = util.Counter() # A Counter is a dict with default 0
-
-    states = mdp.getStates()
-    #self.values = [{ state: 0.0 for state in states }] * ( iterations + 1 )
+    # A Counter is a dict with default 0
+    self.values = util.Counter()
+    
     self.qvalues = { state: {} for state in states }
     self.policy = { state: None for state in states }
+
+    states = mdp.getStates()
     for i in xrange(  1, iterations + 1):
-      # print
-      # print 'iteration', i
       for state in states:
         actions = mdp.getPossibleActions(state)
         v = []
@@ -44,45 +42,43 @@ class ValueIterationAgent(ValueEstimationAgent):
           t = mdp.getTransitionStatesAndProbs(state, action)
           k = [ p * ( mdp.getReward(state, action, nextState) + ( discount * self.values[(i - 1, nextState)] ) ) for nextState, p in t ]
           q = sum(k)
-          # print action, q, t
-          # print k
           self.qvalues[state][action] = q
           v.append([q, action])
-        # print state, v
         if len(v) > 0:
           q, action = max(v, key = lambda x: x[0])
-          # print 'maxvalue: ', state, action, q
           self.values[(i, state)] = q
           self.policy[state] = action
     
   def getValue(self, state):
     """
-      Return the value of the state (computed in __init__).
+    Return the value of the state (computed in `__init__`).
     """
     return self.values[(self.iterations, state)]
 
 
   def getQValue(self, state, action):
     """
-      The q-value of the state action pair
-      (after the indicated number of value iteration
-      passes).  Note that value iteration does not
-      necessarily create this quantity and you may have
-      to derive it on the fly.
+    The q-value of the state action pair
+    (after the indicated number of value iteration
+    passes).  Note that value iteration does not
+    necessarily create this quantity and you may have
+    to derive it on the fly.
     """
     return self.qvalues[state][action]
 
   def getPolicy(self, state):
     """
-      The policy is the best action in the given state
-      according to the values computed by value iteration.
-      You may break ties any way you see fit.  Note that if
-      there are no legal actions, which is the case at the
-      terminal state, you should return None.
+    The policy is the best action in the given state
+    according to the values computed by value iteration.
+    You may break ties any way you see fit.  Note that if
+    there are no legal actions, which is the case at the
+    terminal state, you should return `None`.
     """
     return self.policy[state]
 
   def getAction(self, state):
-    "Returns the policy at the state (no exploration)."
+    """
+    Returns the policy at the state (no exploration).
+    """
     return self.getPolicy(state)
   
